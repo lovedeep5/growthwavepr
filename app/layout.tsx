94b6f02016d244a3d2ae73d3e58978ea6c3a5771
Lovedeep <lovedeep5.abh@gmail.com>
2024-08-21 21:40:06 +0530

    added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,11 +30,32 @@ export default function RootLayout({
         />
         <meta
           property="og:image"
-          content="https://salon24.com/seo/og-image.webp"
+          content="https://thesalon24.com/seo/og-image.jpg"
         />
-        <meta property="og:url" content="https://salon24.com" />
+        <meta property="og:url" content="https://thesalon24.com" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Salon24" />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
